d1d8dda97d7b4f8531a00b85388a8ac10e480372
MIT-FS <irm@us.es>
2024-02-16 18:45:33 +0100

    Añadidas trazas en los handlers

--- business-application-service/src/main/java/com/company/service/jms/ConfiguredJMSSendTaskWorkItemHandler.java
+++ business-application-service/src/main/java/com/company/service/jms/ConfiguredJMSSendTaskWorkItemHandler.java
@@ -8,10 +8,14 @@ import org.kie.api.runtime.process.WorkItemManager;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @Component("External Send Task")
 public class ConfiguredJMSSendTaskWorkItemHandler extends JMSSendTaskWorkItemHandler {
 
     private JmsTemplate jmsTemplate;
+    private static final Logger log = LogManager.getLogger();
 
     public ConfiguredJMSSendTaskWorkItemHandler(ConnectionFactory connectionFactory, JmsTemplate jmsTemplate) {
         super(connectionFactory, null);
@@ -21,8 +25,10 @@ public class ConfiguredJMSSendTaskWorkItemHandler extends JMSSendTaskWorkItemHan
     @Override
     public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
         try {
+            log.info("Voy a pasar a jmsTemplate.send el workITem /n"+workItem);
             jmsTemplate.send("ExternalSignalQueue", (session) -> createMessage(workItem, session));
             manager.completeWorkItem(workItem.getId(), null);
+            log.info("Se ejecuta el metodo executeWorkItem para el workITem /n"+workItem);
         } catch (Exception e) {
             handleException(e);
         }
--- business-application-service/src/main/java/com/company/service/jms/ReceiveJMSEvents.java
+++ business-application-service/src/main/java/com/company/service/jms/ReceiveJMSEvents.java
@@ -6,11 +6,16 @@ import org.jbpm.process.workitem.jms.JMSSignalReceiver;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @Component
 public class ReceiveJMSEvents extends JMSSignalReceiver {
+    private static final Logger log = LogManager.getLogger();
 
     @JmsListener(destination = "ExternalSignalQueue")
     public void processMessage(BytesMessage content) {
+        log.info("Recibido mensaje en ReceiveJMSEvents "+content);
         super.onMessage(content);
     }
     
